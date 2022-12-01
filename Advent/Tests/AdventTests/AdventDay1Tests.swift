588c772e40e71dcddaef7da3e465aabb21b031e6
Ivan Smirnov <ismirnov@live.com>
2022-12-01 19:30:05 +0100

    Add Day1 failing test

--- Advent/Tests/AdventTests/AdventDay1Tests.swift
+++ Advent/Tests/AdventTests/AdventDay1Tests.swift
@@ -1,8 +1,31 @@
 import XCTest
+import Foundation
 @testable import Advent
+
+struct Elf {
+  let itemsCalories: [Int]
+  var totalCalories: Int {
+    return itemsCalories.reduce(0, +)
+  }
+}
+
+struct ElvesGroup {
+  let elves: [Elf]
+    
+  var topCarrierIndex: Int {
+    return 0
+  }
+  
+  var topCarrierLoad: Int {
+    return 0
+  }
+}
 
 final class AdventDay1Tests: XCTestCase {
   func test_mostCaloriesCarriesNthElf_andAmountIsX() {
+    let sut = ElvesGroup(elves: [])
     
+    XCTAssertEqual(sut.topCarrierIndex, -1)
+    XCTAssertEqual(sut.topCarrierLoad, -1)
   }
 }
