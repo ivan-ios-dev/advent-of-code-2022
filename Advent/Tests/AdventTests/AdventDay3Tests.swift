1c1a85c326789a263885e7a4560acb6e655a5aec
Ivan Smirnov <ismirnov@live.com>
2022-12-03 13:21:19 +0100

    Add test_rucksack_canBeInitialized_fromEmptyItemsString

--- Advent/Tests/AdventTests/AdventDay3Tests.swift
+++ Advent/Tests/AdventTests/AdventDay3Tests.swift
@@ -13,6 +13,11 @@ struct Rucksack {
   
   let firstCompartment: Compartment
   let secondCompartment: Compartment
+  
+  init(itemsString: String) {
+    self.firstCompartment = .init(itemsString: "")
+    self.secondCompartment = .init(itemsString: "")
+  }
 }
 
 final class AdventDay3Tests: XCTestCase {
@@ -28,5 +33,10 @@ final class AdventDay3Tests: XCTestCase {
     XCTAssertEqual(sut.items.count, 12)
   }
   
+  func test_rucksack_canBeInitialized_fromEmptyItemsString() {
+    let sut = Rucksack(itemsString: "")
+    XCTAssertEqual(sut.firstCompartment.items.count, 0)
+    XCTAssertEqual(sut.secondCompartment.items.count, 0)
+  }
 }
 
